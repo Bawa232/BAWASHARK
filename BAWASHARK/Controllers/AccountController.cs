8503fa5d33d8a4cbbe1085358d6f735d55c728bb
JBawa <JBawa@HOBO-IT-LT25>
2024-06-26 23:06:06 +0100

    auth

---
+++ BAWASHARK/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using BAWASHARK.Models;
+using BAWASHARK.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BAWASHARK.Controllers
+{ 
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register([FromBody] RegisterDto register)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var appUser = new AppUser
+                {
+                    UserName = register.Username,
+                    Email = register.Email
+                };
+
+                var createdUser = await _userManager.CreateAsync(appUser, register.Password);
+                if (createdUser.Succeeded)
+                {
+                    var userRole = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (userRole.Succeeded)
+                    {
+                        return Ok("user created");
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- BAWASHARK/Data/ApplicationDBContext.cs
+++ BAWASHARK/Data/ApplicationDBContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using BAWASHARK.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace BAWASHARK.Data
 {
@@ -14,5 +15,24 @@ namespace BAWASHARK.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+        }
     }
 }
